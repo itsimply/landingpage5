a8f5680ae7f3bb999183e0350ec5827ba44b56ba
itsimply <lukasz.fedor.projekty@gmail.com>
2018-09-08 13:23:58 +0200

    Landing Page 5 Files with Sass

---
+++ readme.txt
@@ -0,0 +1,2 @@
+=== Landingpage5 ===
+
---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:3,
+        itemsDesktop:[1199,2],
+        itemsDesktopSmall:[979,2],
+        itemsTablet:[768,2],
+        itemsMobile:[600,1],
+        pagination:true,
+        navigation:false,
+        navigationText:["",""],
+        slideSpeed:1000,
+        autoPlay:true
+    });
+});
